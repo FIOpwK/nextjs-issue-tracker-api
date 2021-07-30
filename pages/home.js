dc4ae4d74f7c6236316556e41cf50b2ea880b1c6
Dajuan McDonald <dajuan-mcdonald@lambdastudents.com>
2021-07-30 17:34:03 -0400

    flesh out navigation styling

--- pages/home.js
+++ pages/home.js
@@ -3,18 +3,26 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div class="container">
-      <h3> Home </h3>
-      <nav class="navbar navbar-dark bg-dark">
-        <Link href="/home">
-          
-          <a> Home </a>
-        </Link>
+      <h3> API Issue Tracker </h3>
+      <nav class="navbar sticky-top navbar-dark bg-dark">
+        <div class="container-fluid">
+          <Link href="/home">
+            <a class="navbar-brand"> Home </a>
+          </Link>
 
-        <Link href="/issues">
-     
-          <a> Issues </a>
-        </Link>
+          <Link href="/issues">
+            <a class="navbar-brand"> Issues </a>
+          </Link>
+          <Link href="#">
+            <a class="navbar-brand"> Tracker </a>
+          </Link>
+
+          <Link href="#">
+            <a class="navbar-brand"> News </a>
+          </Link>
+        </div>
       </nav>
+	  
       <div>
         <button class="btn btn-danger"> Open New Issue </button>
       </div>
