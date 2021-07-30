d2318d462deee1d695e439ed272eebf41aa1e7da
Dajuan McDonald <dajuan-mcdonald@lambdastudents.com>
2021-07-30 14:21:01 -0400

    getting pages and routes to template layout req

---
+++ pages/home.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+function Home() { 
+   return (
+<div>
+
+	<a href="pages/issues"> Go to issues</a>
+</div>
+	)
+ }
+
+
+export default Home
---
+++ pages/issues.js
@@ -0,0 +1,8 @@
+function Issue(id, desc) {
+return (
+<div><h2>Issues</h2></div>
+)
+
+}
+
+export default Issue
