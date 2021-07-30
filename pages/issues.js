8fbab28e622bb5faa5451cea69e7e7b7f4331425
Dajuan McDonald <dajuan-mcdonald@lambdastudents.com>
2021-07-30 18:08:15 -0400

    added issues table template for later render

--- pages/issues.js
+++ pages/issues.js
@@ -1,6 +1,41 @@
 function Issue(id, desc) {
 return (
-<div><h2>Issues</h2></div>
+<div class="container">
+    <h2>Issues</h2>
+
+    <div class="table-responsive">
+    <table class="table table-dark">
+  <thead>
+    <tr class="table-active">
+      <th scope="col">ID</th>
+      <th scope="col">Product</th>
+      <th scope="col">Last Modified</th>
+      <th scope="col">Status</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Tracker</td>
+      <td>{Date.now() - 21}</td>
+      <td>Open</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>API</td>
+      <td>{Date.now() + 21}</td>
+      <td>Closed</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Website</td>
+      <td>{Date.now() - 10}</td>
+      <td>Resolved</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+</div>
 )
 
 }
